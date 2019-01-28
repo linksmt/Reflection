d1ec6feb78e4b16a0681e0bf7009f557466a04d0
Matteo <>
2019-01-28 18:10:10 +0100

    Add Xcode project

---
+++ Reflection/Reflection.h
@@ -0,0 +1,18 @@
+//
+//  Reflection.h
+//  Reflection
+//
+//  Created by politom on 28/01/2019.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reflection.
+FOUNDATION_EXPORT double ReflectionVersionNumber;
+
+//! Project version string for Reflection.
+FOUNDATION_EXPORT const unsigned char ReflectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reflection/PublicHeader.h>
+
+
